29889b648fa43d6f1acd847550cf76a2b1e9aa1d
Axel <113966806+conjurs@users.noreply.github.com>
2024-10-23 16:41:20 +0300

    upd

--- back/utils/constants.js
+++ back/utils/constants.js
@@ -1,5 +1,5 @@
 const secretKey = 'ymps';
-const apiKey = '036869811325bcf912fdb2ad0b159b03';
+const apiKey = '036869811325bcf912fdb2ad0b159b03'; // not my key xd
 
 const users = [
   { id: 1, username: 'user1', password: 'password1' },
